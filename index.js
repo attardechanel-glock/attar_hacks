38e1127309ea5416006a1208131b2242b6c7b9e0
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-03-16 13:40:57 +0430

    Reduce commits number

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
+import random from "random";
 import simpleGit from "simple-git";
 
-const path = "./data.json";
-const date = moment().subtract(5, "d").format();
+const makeCommits = (num) => {
+  if (num === 0) return simpleGit().push();
 
-const data = {
-  date: date,
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const path = "./data.json";
+
+  const data = {
+    date: date,
+  };
+  console.log(date);
+  jsonfile.writeFile(path, data, () => {
+    simpleGit()
+      .add(path)
+      .commit(date, { "--date": date }, makeCommits.bind(this, --num));
+  });
 };
 
-jsonfile.writeFile(path, data, () => {
-  simpleGit().add(path).commit(date, { "--date": date }).push();
-});
+makeCommits(100);
+
+
